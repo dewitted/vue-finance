c742bd86c7d3a7b4b29f54a7dbc941e47d783c4f
dewitted <brazauskas9@gmail.com>
2020-02-27 20:37:28 +0200

    current progress, small style changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,6 +12,5 @@ export default {
   .main-header-container {
     width: 100%;
     height: 106px;
-    background-color: blue;
   }
 </style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="main-container">
-    <!-- <div class="finance-buttons">
-      <div
-        class="toAdd"
-        :id="section.name"
-        v-for="section in allFinances"
-        :key="section.name"
-        @click="sectionClick(section.name)"
-      >{{section.name}}</div>
+    <div class="stats-chart"></div>
+    <div class="current-spenditure">
+      <h3>Spent today</h3>
+      <div id="todayTotal" @click="consolell()">{{ currentTotal }}</div>
     </div>
-    <form v-if="activated" id="entry" v-on:submit.prevent="addAmount(); activation()">
-      <input v-model="amount" placeholder="0.00" />
-      <button type="submit">Submit!</button>
-    </form>-->
   </div>
 </template>
 
@@ -23,27 +15,34 @@ export default {
   name: "MainPage",
   data() {
     return {
-      activated: false,
-      selectedCategory: "",
-      amount: ""
+      currentTotal: 0
     };
   },
   methods: {
-    ...mapActions(["updateCategory"]),
-    sectionClick(name) {
-      this.activated = !this.activated;
-      this.selectedCategory = name;
-    },
-    activation() {
-      this.activated = !this.activated;
-    },
-    addAmount() {
-      console.log(this.amount);
-      const updCat = {
-        name: this.selectedCategory,
-        amount: this.amount
-      };
-      this.updateCategory(updCat);
+    consolell() {
+      console.log(this.allFinances[1].amount[0]);
+    }
+  },
+  watch: {
+    allFinances: () => {
+      this.currentTotal =
+        this.currentTotal +
+        function() {
+          let total = 0;
+          for (let i = 0; i < this.allFinances.length; i++) {
+            let total =
+              total +
+              function() {
+                let amountTotal = 0;
+                for (let j = 0; j < this.allFinances[j].amount.length; j++) {
+                  amountTotal = amountTotal + this.allFinances[i].amount[j];
+                }
+                return amountTotal;
+              };
+          }
+          return total;
+        };
+      console.log(this.currentTotal);
     }
   },
   computed: mapGetters(["allFinances"])
@@ -54,79 +53,26 @@ export default {
   .main-container {
     width: 100%;
     height: 600px;
-    background-color: teal;
+    background-color: white;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
     position: relative;
-    .finance-buttons {
+    .stats-chart {
       width: 100%;
       height: 300px;
-      position: relative;
-      .toAdd {
-        width: 70px;
-        height: 70px;
-        background-color: yellow;
-        border-radius: 50%;
-      }
-      #car {
-        position: absolute;
-        top: 5%;
-        left: 25%;
-      }
-      #food {
-        position: absolute;
-        top: 5%;
-        right: 25%;
-      }
-      #party {
-        position: absolute;
-        top: 40%;
-        left: 15%;
-      }
-      #taxes {
-        position: absolute;
-        top: 40%;
-        right: 15%;
-      }
-      #sport {
-        position: absolute;
-        bottom: 5%;
-        left: 25%;
-      }
-      #misc {
-        position: absolute;
-        bottom: 5%;
-        right: 25%;
-      }
     }
-    #entry {
-      width: 80%;
-      height: 250px;
-      background-color: grey;
-      position: absolute;
-      top: 12%;
+    .current-spenditure {
+      width: 100%;
+      height: 300px;
       display: flex;
       flex-direction: column;
-      justify-content: space-between;
+      justify-content: center;
       align-items: center;
-      box-sizing: border-box;
-      padding-top: 40px;
-      padding-bottom: 40px;
-      input {
-        margin: 0;
-        width: 90%;
-        height: 100px;
-        font-size: 4rem;
-        text-align: center;
-      }
-      button {
-        margin: 0;
-        width: 50%;
-        height: 50px;
-        font-size: 1.2rem;
-        text-align: center;
+      #todayTotal {
+        width: 80%;
+        height: 200px;
       }
     }
   }
